e529c052c7343c4e77148ff5ebabfb1bc9d08141
jllort <jaumellort@gmail.com>
2016-12-20 12:14:58 +0100

    Changes in users list

---
+++ app/src/main/java/com/superliga/jllort/bencosuperleague/users/UserListActivity.java
@@ -0,0 +1,37 @@
+package com.superliga.jllort.bencosuperleague.users;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.superliga.jllort.bencosuperleague.R;
+import com.superliga.jllort.bencosuperleague.users.dummy.DummyContent;
+
+public class UserListActivity extends AppCompatActivity implements ItemFragment.OnListFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
+    }
+}
