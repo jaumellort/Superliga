986a5e2ac9cfe5023c0f1421be77ad6768233e3d
jllort <jaumellort@gmail.com>
2016-12-20 17:49:43 +0100

    Changes in users list and user details screen

---
+++ app/src/main/java/com/superliga/jllort/bencosuperleague/dto/Users.java
@@ -0,0 +1,63 @@
+package com.superliga.jllort.bencosuperleague.dto;
+
+/**
+ * Created by jllort on 12/20/2016.
+ */
+
+public class Users {
+
+    private Integer id;
+    private String name;
+    private String gamerTag;
+    private String preferedTeam;
+    private String currentLeagueTeam;
+    private float rating;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGamerTag() {
+        return gamerTag;
+    }
+
+    public void setGamerTag(String gamerTag) {
+        this.gamerTag = gamerTag;
+    }
+
+    public String getPreferedTeam() {
+        return preferedTeam;
+    }
+
+    public void setPreferedTeam(String preferedTeam) {
+        this.preferedTeam = preferedTeam;
+    }
+
+    public String getCurrentLeagueTeam() {
+        return currentLeagueTeam;
+    }
+
+    public void setCurrentLeagueTeam(String currentLeagueTeam) {
+        this.currentLeagueTeam = currentLeagueTeam;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+}
--- app/src/main/java/com/superliga/jllort/bencosuperleague/users/UserDetailActivity.java
+++ app/src/main/java/com/superliga/jllort/bencosuperleague/users/UserDetailActivity.java
@@ -2,6 +2,7 @@ package com.superliga.jllort.bencosuperleague.users;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RatingBar;
 
 import com.superliga.jllort.bencosuperleague.R;
 
@@ -11,5 +12,8 @@ public class UserDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_detail);
+
+        RatingBar rating = (RatingBar) findViewById(R.id.ratingBar);
+        rating.setRating(4.35f);
     }
 }
