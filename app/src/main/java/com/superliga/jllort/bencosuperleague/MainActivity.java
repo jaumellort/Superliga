99119a130a97fcd437d5ab4aa4d1295fd881ff78
jllort <jaumellort@gmail.com>
2016-12-20 12:17:01 +0100

    Changes in users list

---
+++ app/src/main/java/com/superliga/jllort/bencosuperleague/MainActivity.java
@@ -0,0 +1,31 @@
+package com.superliga.jllort.bencosuperleague;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.superliga.jllort.bencosuperleague.users.UserDetailActivity;
+import com.superliga.jllort.bencosuperleague.users.UserListActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button mShowUsersList = (Button) findViewById(R.id.user_list_button);
+        mShowUsersList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent userListIntent = new Intent(MainActivity.this, UserListActivity.class);
+                startActivity(userListIntent);
+            }
+        });
+    }
+
+
+}
